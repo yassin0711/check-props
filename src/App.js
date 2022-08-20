c94c836bb0f52b27f3d16286f689f78970ac23bb
yassin0711 <99025891+yassin0711@users.noreply.github.com>
2022-08-20 15:42:05 +0100

    check piont props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 // import logo from './logo.svg';
 import './App.css';
 import Profile from './Profile/profile';
+
 function App() {
+  const fullName="hello world", bio ="coding", profession ="doctor"
+  const f = () => {
+    alert(fullName)
+  }
+  
   return (
-    <Profile fullName = "hello world" bio = "coding" profession = "doctor" >
+    <div>
+    <Profile fullName = {fullName} bio ={bio}  profession ={profession} f={f}   >
       <img src="./Profile/loffy.png" alt="luffy" />
     </Profile>
+    </div>
+
       
   );
 }
--- src/Profile/profile.js
+++ src/Profile/profile.js
@@ -1,13 +1,26 @@
 import React from "react";
+import PropTypes from "prop-types";
+const styleObject={color:"red",textAlign:'center'}
+
 const Profile = (props) => {
-    return <div>
+    return <div style={styleObject}>
         <h1>name : {props.fullName}</h1>
 
         <h3>profession :{props.profession}</h3>
 
         <h5>bio : {props.bio}</h5>
         
-       
+       <div >{props.cheldren}</div>
+       <button onClick={props.f}>SUBMIT</button>
     </div>
 };
+
+Profile.defaultProps = {
+    fullName: "yassin", profession: "etudiant", bio: "world", f: () =>{return alert("enter your name")}}
+    Profile.propTypes = {
+        fullName: PropTypes.string,
+        profession: PropTypes.string,
+        bio: PropTypes.string,
+        f: PropTypes.func
+       };
 export default Profile;
